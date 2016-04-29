9c2668db0976c20f076418f4e38dd21185938dde
mh-cbon <mh-cbon@users.noreply.github.com>
2016-04-29 14:41:57 +0200

    better error names

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 
 function whichServiceManager (then) {
   require('@mh-cbon/has-systemd')(function (err, systemdPath) {
-    if(!err) return then(null, 'systemd', systemdPath)
-    require('@mh-cbon/has-upstart')(function (err, initctlPath) {
-      if(!err) return then(null, 'upstart', initctlPath)
-      require('@mh-cbon/has-chkconfig')(function (err, chkconfigPath) {
-        if(!err) return then(null, 'chkconfig', chkconfigPath)
-        require('@mh-cbon/has-launchd')(function (err, launchdPath) {
-          if(!err) return then(null, 'launchd', launchdPath)
-          require('@mh-cbon/has-sc')(function (err, scPath) {
-            if(!err) return then(null, 'sc', scPath)
+    if(!err) return then(null, 'systemd', systemdPath);
+    require('@mh-cbon/has-upstart')(function (err1, initctlPath) {
+      if(!err1) return then(null, 'upstart', initctlPath);
+      require('@mh-cbon/has-chkconfig')(function (err2, chkconfigPath) {
+        if(!err2) return then(null, 'chkconfig', chkconfigPath);
+        require('@mh-cbon/has-launchd')(function (err3, launchdPath) {
+          if(!err3) return then(null, 'launchd', launchdPath);
+          require('@mh-cbon/has-sc')(function (err4, scPath) {
+            if(!err4) return then(null, 'sc', scPath);
             return then('unhandled system');
           })
         })
